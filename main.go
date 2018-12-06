a177dec26a1d0629bc2b19f8aa846a15578afa0c
Oliver Walter <oli1111@web.de>
2018-12-06 18:21:27 +0100

    key management

--- main.go
+++ main.go
@@ -12,12 +12,11 @@ import (
 	"time"
 )
 
-
-var key = []byte{ 0x2b, 0x7e,   0x15,   0x16,   0x28,   0xae,   0xd2,   0xa6,   0xab,   0xf7,   0x15,   0x88,   0x09,   0xcf,   0x4f,   0x3c,
-	              0x3e,   0x05,   0xb6,   0x96,   0x55,   0xea,   0x2e,   0xae,   0xe9,   0xee,   0xf1,   0xa2,   0x2f,   0x13,   0x39,   0x99}
+var key = []byte{0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
+	0x3e, 0x05, 0xb6, 0x96, 0x55, 0xea, 0x2e, 0xae, 0xe9, 0xee, 0xf1, 0xa2, 0x2f, 0x13, 0x39, 0x99}
 
 type identi struct {
-	id [23] byte
+	id   [23]byte
 	time time.Time
 }
 
@@ -25,7 +24,7 @@ func (ident identi) equalID(id []byte) bool {
 	if len(id) != 23 {
 		return false
 	}
-	for i := range id  {
+	for i := range id {
 		if id[i] != ident.id[i] {
 			return false
 		}
@@ -34,43 +33,35 @@ func (ident identi) equalID(id []byte) bool {
 }
 
 const TIMEOUT float64 = 30.0
-func (ident identi)timeValid()bool {
-	return  time.Now().Sub(ident.time).Seconds() < TIMEOUT
+
+func (ident identi) timeValid() bool {
+	return time.Now().Sub(ident.time).Seconds() < TIMEOUT
 }
 
 type identList []identi
-var ids = []identi{}
 
-func (ids * identList) filterTimeouts() {
+var ids = identList{}
+
+func (ids *identList) filterTimeouts() {
 
 }
 
-func (ids identList) containsValid( id []byte ) bool {
+func (ids identList) containsValid(id []byte) (bool, int) {
 	for i := range ids {
 		if ids[i].equalID(id) && ids[i].timeValid() {
-			return true
+
+			return true, i
 		}
 	}
-	return false
+	return false, -1
 }
 
-
-func index_handler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w,"<h1>Hello Internet</h1>")
-	fmt.Println(r.Host)
-	fmt.Println(r.Method)
-	fmt.Println(r.ContentLength)
-	fmt.Println("INDEX")
-
-}
-
-func putHandler(w http.ResponseWriter, r *http.Request)  {
+func putHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "PUT" && r.ContentLength == 48 {
 
-
 		//#READ INPUT
-		b := make([]byte,48)
+		b := make([]byte, 48)
 		n, err := r.Body.Read(b)
 
 		if err != nil && err != io.EOF {
@@ -92,7 +83,6 @@ func putHandler(w http.ResponseWriter, r *http.Request)  {
 		mode.CryptBlocks(ciphertext, ciphertext)
 		fmt.Println(ciphertext)
 
-
 		//#DECODE
 		u := ciphertext[:23]
 		sensornum := ciphertext[23]
@@ -103,24 +93,30 @@ func putHandler(w http.ResponseWriter, r *http.Request)  {
 		err = binary.Read(buf1, binary.LittleEndian, &f1)
 		if err != nil {
 			fmt.Println("binary.Read failed:", err)
-			fmt.Fprintf(w,"EV1")
+			fmt.Fprintf(w, "EV1")
 			return
 		}
 		err = binary.Read(buf2, binary.LittleEndian, &f2)
 		if err != nil {
 			fmt.Println("binary.Read failed:", err)
-			fmt.Fprintf(w,"EV2")
+			fmt.Fprintf(w, "EV2")
 			return
 		}
 
 		// U: IDENTIFIER  |  sensornum: Nummer des Sensors  |  f1,f2 Sensor Werte
 		fmt.Println(u, sensornum, f1, f2)
 
-		fmt.Fprintf(w,"OK")
+		bo, idx := ids.containsValid(u)
+		if bo {
+			ids = ids[:idx+copy(ids[idx:], ids[idx+1:])]
+		}
+		fmt.Println(bo)
+
+		fmt.Fprintf(w, "OK")
 		return
- 	}
+	}
 	w.WriteHeader(405)
-	_, err := fmt.Fprintf(w,"NOT ALLOWED")
+	_, err := fmt.Fprintf(w, "NOT ALLOWED")
 
 	if err != nil {
 		fmt.Println("error:", err)
@@ -129,8 +125,8 @@ func putHandler(w http.ResponseWriter, r *http.Request)  {
 
 }
 
-func getHandler(w http.ResponseWriter, r *http.Request)  {
-	_, err := fmt.Fprintf(w,"<h1>Hello Internet</h1>")
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "<h1>Hello Internet</h1>")
 	fmt.Println(r.Host)
 	fmt.Println(r.Method)
 	fmt.Println(r.ContentLength)
@@ -143,7 +139,7 @@ func getHandler(w http.ResponseWriter, r *http.Request)  {
 
 }
 
-func keyHandler(w http.ResponseWriter, r *http.Request)  {
+func keyHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		//CREATE new Rand IDENTIFIER
 		c := aes.BlockSize*2 - 9
@@ -179,7 +175,7 @@ func keyHandler(w http.ResponseWriter, r *http.Request)  {
 	}
 }
 
-func main()  {
+func main() {
 
 	http.HandleFunc("/", http.NotFound)
 	http.HandleFunc("/put", putHandler)
